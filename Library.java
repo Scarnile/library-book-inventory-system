7dc8f6a08f4268f0e222bfc8d8820d1e17c2f138
Scarnile <198330506+Scarnile@users.noreply.github.com>
2025-12-04 06:20:30 +0800

    Add book copy to LibraryUser's account

--- Library.java
+++ Library.java
@@ -1,21 +1,31 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Library {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Book> books = new ArrayList<>();
         LibraryUser libraryUser = new LibraryUser();
 
         books.add(new Book("Lord of the Rings", "John Tolkien", 1925, 2));
-        books.add(new Book("Harry Potter", "J.K Rowling", 1980, 1));
+        books.add(new Book("Harry Potter", "J.K Rowling", 1980, 0));
 
         System.out.println("Would you like to borrow any of the books below?");
         ShowAllBooks(books);
+        int answer = scanner.nextInt();
+        BorrowBook(libraryUser, books, answer);
+        System.out.println(libraryUser.books.getFirst().title);
     }
 
-    static void BorrowBook(Book book) {
+    static void BorrowBook(LibraryUser libraryUser, ArrayList<Book> books, int index) {
         // If book still has library copies, borrow the book
+
+        Book book = books.get(index);
+
         if (book.libraryCopies > 0) {
             book.libraryCopies -= 1;
+            libraryUser.books.add(book);
+            System.out.println("You have successfully borrowed " + book.title);
         } else {
             System.out.println("There are no more copies left of " + book.title);
         }
