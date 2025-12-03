5dc6e6676e1a23acaa5f0d93d8f442bc925be1a3
Scarnile <198330506+Scarnile@users.noreply.github.com>
2025-12-03 22:13:18 +0800

    Make book class

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Book book = new Book();
 
+        System.out.println(book.title);
     }
+
+}
+
+class Book {
+    String title;
+    String author;
+    int year;
+    int copies;
 }
