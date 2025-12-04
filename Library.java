4aa33eaf6cf803131b4f093d0b1873ce5b8c2233
Scarnile <198330506+Scarnile@users.noreply.github.com>
2025-12-04 20:13:47 +0800

    Add ReturnBook method

--- Library.java
+++ Library.java
@@ -12,8 +12,8 @@ public class Library {
         scanner.useDelimiter(System.lineSeparator());
 
         // Initialize Books
-        books.add(new Book("Lord of the Rings", "John Tolkien", 1925, 2));
-        books.add(new Book("Harry Potter", "J.K Rowling", 1980, 0));
+        books.add(new Book("Lord of the Rings", "John Tolkien", 1925, 1));
+        books.add(new Book("Harry Potter", "J.K Rowling", 1980, 1));
 
         // System Loop
         while (isSystemRunning) {
@@ -38,6 +38,7 @@ public class Library {
                 BorrowBook(libraryUser, books, scanner);
                 break;
             case "1":
+                ReturnBook(libraryUser, books, scanner);
                 break;
             case "2":
                 ShowAllBooks(books);
@@ -52,31 +53,62 @@ public class Library {
 
     }
 
-    static void BorrowBook(LibraryUser libraryUser, ArrayList<Book> books, Scanner scanner) {
+    static void BorrowBook(LibraryUser libraryUser, ArrayList<Book> libraryBooks, Scanner scanner) {
 
         // Ask user for the index of the book
         System.out.println("\nWould you like to borrow any of the books below?");
-        ShowAllBooks(books);
-        System.out.print("Book Index: ");
-        int index = scanner.nextInt();
+        ShowAllBooks(libraryBooks);
+        System.out.print("\nInsert Book Index: ");
+        int inputIndex = scanner.nextInt();
 
         // If book has an invalid index
-        if (index >= books.size()) {
+        if (inputIndex >= libraryBooks.size()) {
             System.out.println("Invalid book index, try again");
             return;
         } else {
-            Book book = books.get(index);
+            Book selectedBook = libraryBooks.get(inputIndex);
 
-            // If book still has library copies, borrow the book
-            if (book.libraryCopies > 0) {
-                book.libraryCopies -= 1;
-                libraryUser.books.add(book);
-                System.out.println("You have successfully borrowed " + book.title);
+            // If selectedBook still has library copies, borrow the selectedBook
+            if (selectedBook.libraryCopies > 0) {
+                selectedBook.libraryCopies -= 1;
+                libraryUser.books.add(selectedBook);
+                System.out.println("You have successfully borrowed " + selectedBook.title);
             } else {
-                System.out.println("There are no more copies left of " + book.title);
+                System.out.println("There are no more copies left of " + selectedBook.title);
             }
             System.out.println(""); // Print empty line
         }
+    }
+
+    static void ReturnBook(LibraryUser libraryUser, ArrayList<Book> libraryBooks, Scanner scanner) {
+        // Display all the library user's books
+        if (!libraryUser.books.isEmpty()) {
+            System.out.println("\nWhich book do you want to return?: ");
+            ShowAllBooks(libraryUser.books);
+
+            int inputIndex = scanner.nextInt();
+
+            // If book has an invalid index
+            if (inputIndex >= libraryUser.books.size()) {
+                System.out.println("Invalid book index, try again");
+                return;
+            } else {
+                // Find LibraryUser's book in Library and add a copy
+                Book selectedBook = libraryUser.books.get(inputIndex);
+
+                for (Book libraryBook : libraryBooks) {
+                    // Book to return matches the library data
+                    if (selectedBook.title == libraryBook.title) {
+                        libraryBook.libraryCopies++;
+                        libraryUser.books.remove(selectedBook);
+                        System.out.println("Successfully returned " + selectedBook.title + "\n");
+                    }
+                }
+            }
+        } else {
+            System.out.println("Sorry, you haven't borrowed any books from the library yet\n");
+        }
+
     }
 
     static void ShowAllBooks(ArrayList<Book> books) {
